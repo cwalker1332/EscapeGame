b34f9726030be78f4436a376b23ec61375ded7c7
Student <student@DC-TECH-05.local>
2020-11-11 20:42:22 -0600

    no changes

---
+++ android/src/com/escape/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.escape.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.escape.game.MyEscapeGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyEscapeGame(), config);
+	}
+}
